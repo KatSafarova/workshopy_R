55526355cbef8f20c58d5b727af963f2b89f8f73
KatSafarova <kat.svaaa@gmail.com>
2026-03-29 10:58:19 +0200

    Add files via upload

---
+++ 01_klavesove_zkratky.R
@@ -0,0 +1,28 @@
+
+# vybrané užitečné klávesové zkratky -------------------------------------------------------
+
+# zobrazí klávesové zkratky                     Alt + Shift + k 
+# zobrazí nápovědu u funkcí                     F1 
+# ukládá                                        Ctrl + S
+# vyhledávání                                   Ctrl + F
+# spustí kód, ve kterém jsme                    Ctrl + Enter   
+# spustí kód od začátku k řádku, v němž jsme    Ctrl + Alt + B    
+# spustí kód od  řádku, v němž jsme do konce    Ctrl + Alt + E    
+# spustí kód v celém skriptu                    Ctrl + Alt + R 
+# pipe %>%                                      Shift + Ctrl + M
+# operátor "nebo" |                             AltGr + W
+# operátor "a" &                                AltGr + C
+# zpětné lomítko \                              AltGr + Q
+# přiřazení <-                                  Alt + -
+# názvy sekcí v kódu                            Ctrl + Shift + R
+# křížek pro komentování                        AltGr + X
+# zakomentování a odkomentování řádků           Ctrl + Shift + C
+# hranaté závorky []                            AltGr + F / AltGr + G
+# složené závorky {}                            AltGr + B / AltGr + N
+# $                                             AltGr + ů
+# #                                             AltGr + x
+# *                                             AltGr + -
+# <                                             AltGr + <
+# >                                             AltGr + <
+
+ 
